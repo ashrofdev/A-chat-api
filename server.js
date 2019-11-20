6de045420bc18d7c9e48997d109e2bf40a820a0f
First Last <salmanashraf853@gmail.com>
2019-11-20 20:38:52 +0100

    b

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ let time = ()=>{
 
 
 
+
 app.post('/send-message', (req, res)=>{
     db('message')
     .returning('*')
@@ -77,4 +78,37 @@ app.post('/send-message', (req, res)=>{
     }).catch(err=> res.json('unable to send'))
 })
 
-app.listen(process.env.PORT)
+app.post('/a', (req, res)=>{
+    const test = {
+      name: req.body.name
+    }
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      
+      auth: {
+        user: "ashsal2001@gmail.com",
+        pass: "salmanashrafatmagul"
+      },
+      tls: {
+        
+        rejectUnauthorized: false
+      }
+    });
+    
+    
+    let info = transporter.sendMail({
+        from: 'ashsal2001@gmail.com', 
+        to: 'ashsall115@gmail.com', 
+        subject: 'PRODUCT REQUEST ✔', 
+        text: 'Hello world?', 
+        html: '<b>A client requested your product</b> <p>Wow, finally youve got a customer</p>' // html body
+    })
+    
+    console.log('Message sent: %s', info.messageId,);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    
+  })
+  
+
+app.listen(process.env.PORT || 3001)
